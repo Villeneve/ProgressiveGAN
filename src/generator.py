b68304d21e3749e7d97bfd565879475c0d7e00d7
villeneve <villeneveoliveira@gmail.com>
2025-08-24 14:24:34 -0300

    feat: add contructor for model

--- src/generator.py
+++ src/generator.py
@@ -19,6 +19,7 @@ def block_conv(x,n_filters):
     x = lay.UpSampling2D(
         size=(2,2)
     )(x)
+    return x
 
 def toRGB(x):
     x = lay.Conv2D(
@@ -27,8 +28,8 @@ def toRGB(x):
         strides=(1,1),
         padding='same',
         activation='tanh',
-        name='toRGB'
-    )
+    )(x)
+    return x
 
 def fade_in(from_layer,to_layer,alpha=0.):
     return (1-alpha)*from_layer + alpha*to_layer
@@ -36,11 +37,19 @@ def fade_in(from_layer,to_layer,alpha=0.):
 def create_generator(input_shape=128):
     inputs = lay.Input(shape=(input_shape,))
     x = inputs
-    x = lay.Dense(4*4*512,activation='leaky_relu')(inputs)
-    out4x4 = lay.Reshape(target_shape=(4,4,512))(x)
 
-    out8x8 = block_conv(out4x4,256)
-    out16x16 = block_conv(out8x8,128)
-    out32x32 = block_conv(out16x16,64)
+    x = lay.Dense(4*4*512,activation='leaky_relu')(inputs)
+    x = lay.Reshape(target_shape=(4,4,512))(x)
+    out4x4 = toRGB(x)
+
+    x = block_conv(x,256)
+    out8x8 = toRGB(x)
+
+    x = block_conv(x,128)
+    out16x16 = toRGB(x)
+
+    x = block_conv(x,64)
+    out32x32 = toRGB(x)
+
     return keras.Model(inputs,[out4x4,out8x8,out16x16,out32x32])
 
