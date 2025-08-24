b7471445314876570104bf9b4f95476d12054384
villeneve <villeneveoliveira@gmail.com>
2025-08-24 14:06:36 -0300

    update files and archteture

--- src/generator.py
+++ src/generator.py
@@ -1,4 +1,5 @@
 import keras.layers as lay
+import keras
 
 def block_conv(x,n_filters):
     x = lay.Conv2D(
@@ -19,9 +20,27 @@ def block_conv(x,n_filters):
         size=(2,2)
     )(x)
 
+def toRGB(x):
+    x = lay.Conv2D(
+        filters=3,
+        kernel_size=(1,1),
+        strides=(1,1),
+        padding='same',
+        activation='tanh',
+        name='toRGB'
+    )
+
+def fade_in(from_layer,to_layer,alpha=0.):
+    return (1-alpha)*from_layer + alpha*to_layer
+
 def create_generator(input_shape=128):
-    inputs = lay.Input(shape=(128,))
-    x = 
+    inputs = lay.Input(shape=(input_shape,))
+    x = inputs
     x = lay.Dense(4*4*512,activation='leaky_relu')(inputs)
-    out1 = lay.Reshape(target_shape=(4,4,512))(x)
+    out4x4 = lay.Reshape(target_shape=(4,4,512))(x)
+
+    out8x8 = block_conv(out4x4,256)
+    out16x16 = block_conv(out8x8,128)
+    out32x32 = block_conv(out16x16,64)
+    return keras.Model(inputs,[out4x4,out8x8,out16x16,out32x32])
 
