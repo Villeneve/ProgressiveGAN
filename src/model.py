8711ce8fe0565c20a394b28fc0337ac05381194b
villeneve <villeneveoliveira@gmail.com>
2025-09-02 20:57:10 -0300

    feat: finish the generator archtecture

--- src/model.py
+++ src/model.py
@@ -58,3 +58,36 @@ class Generator(keras.Model):
             self.fade_in.append(
                 Fade_in(name=f'fade_in({i})')
             )
+        
+        # Cérebro do gerador
+        self.brain = lay.Dense(4*4*512,activation='leaky_relu',name='Brain')
+        self.reshape = lay.Reshape((4,4,512),name='Reshape')
+
+    def call(self, inputs, *args, **kwargs):
+        
+        # Forward pass para 4x4;
+        x = self.brain(inputs)
+        x = self.reshape(x)
+        x = self.conv2[0](x)
+        x = self.conv2[1](x)
+        s4 = self.toRGB[0](x)
+
+        up8 = self.usample[0](x)
+        x = self.conv2[2](up8)
+        x = self.conv2[3](x)
+        x = self.toRGB[1](x)
+        s8 = self.fade_in[0]([self.toRGB[0](up8),x])
+
+        up16 = self.usample[1](x)
+        x = self.conv2[4](up16)
+        x = self.conv2[5](x)
+        x = self.toRGB[2](x)
+        s16 = self.fade_in[1]([self.toRGB[1](up16),x])
+
+        up32 = self.usample[2](x)
+        x = self.conv2[6](up32)
+        x = self.conv2[7](x)
+        x = self.toRGB[3](x)
+        s32 = self.fade_in[2]([self.toRGB[2](up32),x])
+        
+        return [s4,s8,s16,s32]
