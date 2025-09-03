9ec611556178eab3de01684018b7ad95bd27535d
villeneve <villeneveoliveira@gmail.com>
2025-09-03 12:08:11 -0300

    feat: add switch_case functionality

--- src/model.py
+++ src/model.py
@@ -59,35 +59,77 @@ class Generator(keras.Model):
         # Cérebro do gerador
         self.brain = lay.Dense(4*4*512,activation='leaky_relu',name='Brain')
         self.reshape = lay.Reshape((4,4,512),name='Reshape')
+        self.stage = tf.Variable(0, dtype=tf.uint32, trainable=False)
 
     def call(self, inputs, *args, **kwargs):
         
         # Forward pass para 4x4;
-        x = self.brain(inputs)
-        x = self.reshape(x)
-        x = self.conv2[0](x)
-        x = self.conv2[1](x)
-        s4 = self.toRGB[0](x)
+        def forward_4x4(inputs):
+            x = self.brain(inputs)
+            x = self.reshape(x)
+            x = self.conv2[0](x)
+            x = self.conv2[1](x)
+            return self.toRGB[0](x)
 
-        up8 = self.usample[0](x)
-        x = self.conv2[2](up8)
-        x = self.conv2[3](x)
-        x8 = self.toRGB[1](x)
-        s8 = self.fade_in[0]([self.toRGB[0](up8),x8])
-
-        up16 = self.usample[1](x)
-        x = self.conv2[4](up16)
-        x = self.conv2[5](x)
-        x16 = self.toRGB[2](x)
-        s16 = self.fade_in[1]([self.toRGB[1](up16),x16])
-
-        up32 = self.usample[2](x)
-        x = self.conv2[6](up32)
-        x = self.conv2[7](x)
-        x32 = self.toRGB[3](x)
-        s32 = self.fade_in[2]([self.toRGB[2](up32),x32])
+        def forward_8x8(inputs):
+            x = self.brain(inputs)
+            x = self.reshape(x)
+            x = self.conv2[0](x)
+            x = self.conv2[1](x)
+            x = self.usample[0](x)
+            # Old path
+            s4 = self.toRGB[0](x)
+            # New path
+            x = self.conv2[2](x)
+            x = self.conv2[3](x)
+            s8 = self.toRGB[1](x)
+            return self.fade_in[0]([s4,s8])
         
-        return [s4,s8,s16,s32]
+        def forward_16x16(inputs):
+            x = self.brain(inputs)
+            x = self.reshape(x)
+            x = self.conv2[0](x)
+            x = self.conv2[1](x)
+            x = self.usample[0](x)
+            x = self.conv2[2](x)
+            x = self.conv2[3](x)
+            x = self.usample[1](x)
+            # Old path
+            s8 = self.toRGB[1](x)
+            # New path
+            x = self.conv2[4](x)
+            x = self.conv2[5](x)
+            s16 = self.toRGB[2](x)
+            return self.fade_in[1]([s8,s16])
+        
+        def forward_32x32(inputs):
+            x = self.brain(inputs)
+            x = self.reshape(x)
+            x = self.conv2[0](x)
+            x = self.conv2[1](x)
+            x = self.usample[0](x)
+            x = self.conv2[2](x)
+            x = self.conv2[3](x)
+            x = self.usample[1](x)
+            x = self.conv2[4](x)
+            x = self.conv2[5](x)
+            x = self.usample[2](x)
+            # Old path
+            s16 = self.toRGB[2](x)
+            # New path
+            x = self.conv2[6](x)
+            x = self.conv2[7](x)
+            s32 = self.toRGB[3](x)
+            return self.fade_in[2]([s16,s32])
+        
+        branch_fn =[
+            lambda: forward_4x4(inputs),
+            lambda: forward_8x8(inputs),
+            lambda: forward_16x16(inputs),
+            lambda: forward_32x32(inputs)
+        ]
+        
+        return tf.switch_case(self.stage.read_value(),branch_fn)
     
 
 class Discriminator(keras.Model):
@@ -96,6 +138,8 @@ class Discriminator(keras.Model):
 
         self.dsample = [lay.AvgPool2D((2,2),name=f'downsample{i}') for i in [16,8,4]]
         self.brain = lay.Dense(1,activation='sigmoid',name='Brain')
+        self.fade_in = [Fade_in(name=f'fade_in({i})') for i in ['4_8','8_16','16_32']]
+        self.stage = tf.Variable(0, dtype=tf.uint32, trainable=False)
         self.fromRGB = [
             lay.Conv2D(
                 filters=128,
@@ -132,3 +176,15 @@ class Discriminator(keras.Model):
             )
 
         self.flat = lay.Flatten(name='flat_layer')
+
+    def call(self, inputs, *args, **kwargs):
+        def forward_4x4(inputs):
+            x = self.fromRGB[0](inputs)
+            x = self.conv2[0](x)
+            x = self.conv2[1](x)
+            x = self.flat(x)
+            x = self.brain(x)
+            return x
+        
+        return tf.switch_case(self.stage.read_value(),[lambda: forward_4x4(inputs)])
+        
